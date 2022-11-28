269520d2a0dc6894bdd0340d8ddb5ce52437517a
vineeth <15072cm031@gmail.com>
2022-11-28 10:46:34 +0000

    all test cases are passed

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="bg-container">
+    <h1 class="main-heading">Super Over League</h1>
+    <div class="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
